b854f91a4dfe58ea00618df268424ad6ae87eff1
jsbryce1 <jedi1991@hotmail.com>
2018-09-07 19:52:41 -0600

    try again

---
+++ src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Jumbotron.css";
+
+
+const Jumbotron = () => (
+<div className="jumbotron jumbotron-fluid">
+  <div className="container text-center">
+      <h1 className="display-4">PokeMemory</h1>
+    <p className="lead">Click on any pokemon... just dont click on the same one twice :) </p>
+  </div>
+</div>
+);
+
+export default Jumbotron;
